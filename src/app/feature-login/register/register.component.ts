73589dba1103ce959fa12caf1c0f88c0d2be4f2f
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-04-10 16:44:42 +0700

    change some UX-UI

--- src/app/feature-login/register/register.component.ts
+++ src/app/feature-login/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../_services/auth.service";
+import {AuthService} from "../securiy/_services/auth.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
